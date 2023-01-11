cb0c515d701998c5037974df47ca3aff137f3172
Yanina Gandolfi <yaninagandolfi@gmail.com>
2023-01-10 22:38:19 -0300

    primer commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+/*
+do {
+    nombreDeUsuario = prompt("Hola, bienvenido al calculador de porciones, ingresá tu nombre por favor");
+    if (nombreDeUsuario == ""){
+        alert ("ingresá un nombre por favor")
+
+    }
+} while (nombreDeUsuario == "");
+
+
+let cantidad_ingredientes = parseInt(prompt("¿Cuántos ingredientes contiene tu receta?"));
+
+for (let ingrediente = 1; ingrediente <= cantidad_ingredientes; ingrediente++) {
+    let nombreIngrediente = prompt("ingrese nombre del ingrediente")
+    if (escondicionSalida("ESC")) {
+        alert("Nos vemos!")
+        break
+    }
+
+}
+
+
+recetaOriginal = parseInt(prompt("HOLA " + nombreDeUsuario + ", para cuántas personas es tu receta original?"));
+recetaAcalcular = parseInt(prompt("Para cuantas personas querés convertir los valores de esa receta?"));
+cuantosIngredientes = parseInt(prompt("¿Cuántos ingredientes contiene tu receta?"));
+
+
+
+*/
+
+
